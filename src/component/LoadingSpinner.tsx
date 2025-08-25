31724653a0341b4e98680f8ab60b9120f7902d32
Ilo <hei.iloniaina@gmail.com>
2025-08-25 18:17:16 +0300

    ADD: Implement LoadingSpinner component and integrate it into LogIn and SignUp forms

---
+++ src/component/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+
+export default function LoadingSpinner() {
+    return (
+        <div className="flex items-center justify-center">
+            <div className="w-6 h-6 border-4 border-gray-200 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+    )
+}
