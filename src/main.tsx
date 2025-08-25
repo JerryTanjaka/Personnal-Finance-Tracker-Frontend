7e555f92f88a42217a5a6f1f117b90b21c6063a0
Vaniaina <hei.vaniaina@gmail.com>
2025-08-25 13:36:35 +0300

    ADD: StatCard

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+
 import {
   createBrowserRouter,
   Navigate,
   RouterProvider,
 } from 'react-router-dom';
+
 import LogIn from './pages/LogIn.tsx';
 import SignUp from './pages/SignUp.tsx';
+
+import Dashboard from './pages/Dashboard.tsx';
 
 const router = createBrowserRouter([
   { path: '/', element: <Navigate to="/login" /> },
   { path: '/login', element: <LogIn /> },
   { path: '/signup', element: <SignUp /> },
-  { path: '/dashboard', element: <App /> },
+  { path: '/dashboard', element: <Dashboard /> }
 ]);
 
 createRoot(document.getElementById('root')!).render(
