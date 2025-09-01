f12b5a0fac4c3035164230c4048bfe7068c596c6
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-09-01 23:53:20 +0300

    style : improve login design

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,5 +1,8 @@
 import { useState, type FormEvent } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import applelogo from '../assets/Apple.png';
+import googleLogo from '../assets/Google.png';
+import { FaUser, FaLock } from 'react-icons/fa';
 import ErrorMessage from '../components/UI/ErrorMessage.tsx';
 import LoadingSpinner from '../components/UI/LoadingSpinner.tsx';
 
@@ -64,6 +67,7 @@ export default function LogIn() {
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="bg-white px-4 py-8 shadow-2xl sm:rounded-lg sm:px-10">
                     <form className="space-y-6" onSubmit={handleLogIn}>
+                        
                         <div>
                             <label
                                 htmlFor="email"
@@ -71,7 +75,10 @@ export default function LogIn() {
                             >
                                 Email address
                             </label>
-                            <div className="mt-1">
+                            <div className="mt-1 relative">
+                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <FaUser className="text-gray-400" />
+                                </div>
                                 <input
                                     id="email"
                                     name="email"
@@ -80,12 +87,13 @@ export default function LogIn() {
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
                                     required
-                                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none sm:text-sm"
+                                    className="block w-full appearance-none rounded-md border border-gray-300 px-10 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none sm:text-sm"
                                     placeholder="you@example.com"
                                 />
                             </div>
                         </div>
 
+                        
                         <div>
                             <label
                                 htmlFor="password"
@@ -93,23 +101,25 @@ export default function LogIn() {
                             >
                                 Password
                             </label>
-                            <div className="mt-1">
+                            <div className="mt-1 relative">
+                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <FaLock className="text-gray-400" />
+                                </div>
                                 <input
                                     id="password"
                                     name="password"
                                     type="password"
                                     autoComplete="current-password"
-                                    required
                                     value={password}
-                                    onChange={(e) =>
-                                        setPassword(e.target.value)
-                                    }
-                                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none sm:text-sm"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                    className="block w-full appearance-none rounded-md border border-gray-300 px-10 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none sm:text-sm"
                                     placeholder="••••••••"
                                 />
                             </div>
                         </div>
 
+                        
                         <div className="flex items-center justify-between">
                             <div className="flex items-center">
                                 <input
@@ -125,7 +135,6 @@ export default function LogIn() {
                                     Remember me
                                 </label>
                             </div>
-
                             <div className="text-sm">
                                 <a
                                     href="#"
@@ -136,46 +145,62 @@ export default function LogIn() {
                             </div>
                         </div>
 
+                        
                         <div>
                             <button
                                 type="submit"
-                                className="flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-800 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none"
+                                className="flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-neutral-950 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-neutral-800 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none"
                             >
                                 {loading ? <LoadingSpinner /> : 'Sign in'}
                             </button>
                         </div>
                     </form>
 
-                    <div className="mt-6">
-                        <div className="relative">
-                            <div className="absolute inset-0 flex items-center">
-                                <div className="w-full border-t border-gray-300" />
-                            </div>
-                            <div className="relative flex justify-center text-sm">
-                                <span className="bg-white px-2 text-gray-500">
-                                    New to Personal Finance Tracker?
-                                </span>
-                            </div>
+                    
+                    <div className="mt-6 relative">
+                        <div className="absolute inset-0 flex items-center">
+                            <div className="w-full border-t border-gray-300" />
                         </div>
+                        <div className="relative flex justify-center text-sm">
+                            <span className="bg-white px-2 text-gray-500">
+                                New to Personal Finance Tracker?
+                            </span>
+                        </div>
+                    </div>
 
-                        <div className="mt-6">
-                            <Link to="/signup">
-                                <button className="flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none">
-                                    Create an account
-                                </button>
-                            </Link>
-                        </div>
-                        <div className="mt-4 flex justify-center text-gray-500">
-                            <h3>or with </h3>
-                        </div>
-                        <div className="mt-2 flex gap-4">
-                            <button className="flex-1 rounded-lg border border-gray-300 bg-white py-2 font-semibold text-black transition hover:bg-blue-50">
-                                Google
+                    
+                    <div className="mt-6">
+                        <Link to="/signup">
+                            <button className="flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none">
+                                Create an account
                             </button>
-                            <button className="flex-1 rounded-lg border border-gray-300 bg-white py-2 font-semibold text-black transition hover:bg-gray-200">
-                                 Apple
-                            </button>
-                        </div>
+                        </Link>
+                    </div>
+
+                    
+                    <div className="mt-4 flex justify-center text-gray-500">
+                        <h3>or with </h3>
+                    </div>
+                    <div className="mt-2 flex gap-4">
+                        
+                        <button className="flex flex-1 items-center justify-center gap-2 rounded-lg border border-gray-300 bg-white py-2 font-semibold text-black transition hover:bg-blue-50">
+                            <img
+                                src={googleLogo}
+                                alt="Google Logo"
+                                className="h-5 w-5"
+                            />
+                            Google
+                        </button>
+
+                        
+                        <button className="flex flex-1 items-center justify-center gap-2 rounded-lg border border-gray-300 bg-white py-2 font-semibold text-black transition hover:bg-gray-200">
+                            <img
+                                src={applelogo}
+                                alt="Apple Logo"
+                                className="h-5 w-5"
+                            />
+                            Apple
+                        </button>
                     </div>
                 </div>
             </div>
